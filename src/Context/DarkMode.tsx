c57c4b087014d9535b8cf995a329dbb88aea1c21
agu <iba.agustin@gmail.com>
2023-01-11 16:54:51 -0300

    darkmode context added

---
+++ src/Context/DarkMode.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+const DarkModeContext = React.createContext<any>(null);
+
+interface Props {
+  children: any;
+}
+const DarkModeProvider: FC<Props> = (props) => {
+  const [darkMode, setDarkMode] = React.useState<boolean>(false);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {props.children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export { DarkModeProvider, DarkModeContext };
--- src/Header/Navbar.tsx
+++ src/Header/Navbar.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from "react";
+import { DarkModeContext } from "../Context/DarkMode";
 
 export const Navbar: FC = () => {
+  const { darkMode, toggleDarkMode } = React.useContext(DarkModeContext);
+  console.log(darkMode);
+
+  const handleClick = () => {
+    toggleDarkMode();
+  };
   return (
-    <nav>
-      <ul>
+    <nav
+      className={`${
+        darkMode ? "bg-slate-800 text-slate-50" : "bg-teal-100"
+      } flex flex-row justify-around p-4`}
+    >
+      <ul className="flex between">
         <li>back top</li>
         <li>let's connect</li>
       </ul>
-      <div>
-        <button>dark</button>
+      <div className="flex space-x-8">
+        <button onClick={handleClick}>dark</button>
         <button>EN</button>
       </div>
     </nav>
