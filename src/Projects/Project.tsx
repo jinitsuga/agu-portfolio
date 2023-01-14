edd14e1d48d81a1edaccef26412c2256aa39a860
agu <iba.agustin@gmail.com>
2023-01-14 13:58:26 -0300

    project component setup

--- src/Projects/Project.tsx
+++ src/Projects/Project.tsx
@@ -4,8 +4,23 @@ interface Props {
   img: string;
   name: string;
   code: string;
+  description: string;
+  live: string;
 }
 
-export const Project: FC<Props> = () => {
-  return <div></div>;
+export const Project: FC<Props> = ({ img, name, code, live, description }) => {
+  return (
+    <div>
+      <img src={img}></img>
+      <span>{description}</span>
+      <ul>
+        <li>
+          <a href={code}></a>
+        </li>
+        <li>
+          <a href={live}></a>
+        </li>
+      </ul>
+    </div>
+  );
 };
