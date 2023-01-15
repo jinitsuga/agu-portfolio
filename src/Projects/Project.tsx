bb03981ba90a55a883ee4831b50ce4f24be3b0bb
agu <iba.agustin@gmail.com>
2023-01-15 15:35:53 -0300

    project style details and urls

--- src/Projects/Project.tsx
+++ src/Projects/Project.tsx
@@ -14,22 +14,32 @@ export const Project: FC<Props> = ({ img, name, code, live, description }) => {
   const { darkMode } = React.useContext(DarkModeContext);
 
   return (
-    <div className="flex flex-col items-center justify-center p-4 hover:shadow-sm hover:shadow-stone-900">
+    <div className="flex flex-col items-center justify-center p-4 hover:shadow-md hover:shadow-stone-900">
       <h4 className="mb-2 text-xl">{name}</h4>
       <img className="max-w-[300px] max-h-[200px] mb-2" src={img}></img>
       <span className="max-w-[350px]">{description}</span>
-      <ul className="flex gap-4 mt-2">
+      <ul className="flex gap-4 mt-2 text-xl">
         <li>
           <a target="_blank" href={code}>
             {darkMode ? (
-              <i className="devicon-github-original text-3xl border-b-2 border-transparent hover:border-stone-100"></i>
+              <i className="devicon-github-original p-1 border-b-2 border-transparent hover:border-stone-100"></i>
             ) : (
-              <i className="devicon-github-original colored border-b-2 border-transparent hover:border-stone-900"></i>
+              <i className="devicon-github-original p-1 colored border-b-2 border-transparent hover:border-stone-900"></i>
             )}
           </a>
         </li>
         <li>
-          <a href={live}>Live</a>
+          <a
+            target="_blank"
+            className={`${
+              darkMode
+                ? "p-1 border-b-2 border-transparent hover:border-stone-100"
+                : "border-b-2 border-transparent hover:border-stone-900"
+            }`}
+            href={live}
+          >
+            Live
+          </a>
         </li>
       </ul>
     </div>
