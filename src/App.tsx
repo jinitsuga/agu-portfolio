fdf255b770f21238bb94588e598be66a1db737c1
agu <iba.agustin@gmail.com>
2023-04-12 09:31:32 -0300

    added blog components, router with its routes, testing loader data from req

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { DarkModeProvider } from "./Context/DarkMode";
 import { LangProvider } from "./Context/Language";
 import { Container } from "./Container";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Blog } from "./Blog/Blog";
 import "./App.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Container /> },
+  {
+    path: "/blog",
+    loader: async () => {
+      // import JSON with blog posts from API
+    },
+    element: <Blog />,
+  },
+]);
 
 function App() {
   return (
---
+++ src/Blog/Blog.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Blog = () => {
+  return (
+    <section>
+      <h2>Random blog posts :) </h2>
+    </section>
+  );
+};
