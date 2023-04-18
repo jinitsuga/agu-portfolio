7cb28e0f4368624c3eb7a04a538accc1ec8e4be8
agu <iba.agustin@gmail.com>
2023-04-17 21:57:54 -0300

    blog item components are fetched and rendered

--- src/Blog/Blog.tsx
+++ src/Blog/Blog.tsx
@@ -13,24 +13,27 @@ export const Blog: FC = () => {
 
   React.useEffect(() => {
     getPosts()
-      .then(setPosts)
+      .then((res) => setPosts(res))
       .catch(() => setPosts([]));
   }, []);
 
   console.log(posts);
 
-  const postsList = posts?.map((post) => {
-    const description =
-      post.content.length > 150 ? post.content.slice(0, 150) : post.content;
+  const postsList =
+    posts &&
+    posts.map((post) => {
+      const description =
+        post.content.length > 150 ? post.content.slice(0, 150) : post.content;
 
-    return (
-      <BlogItem key={post.id} title={post.name} description={description} />
-    );
-  });
+      return (
+        <BlogItem key={post.id} title={post.name} description={description} />
+      );
+    });
 
   return (
-    <section className="flex flex-column text-white w-screen h-screen">
+    <section className="flex flex-col text-white w-screen h-screen items-center">
       <h2 className="text-4xl text-left">Posts </h2>
+      <div className="flex flex-col justify-center">{postsList}</div>
     </section>
   );
 };
